99f32bd3984523a724a7e4d4550648c4c1784701
yuhangwang <stevenaura@outlook.com>
2017-03-08 15:42:58 -0600

    update main.go to handle lack of arguments

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
     "os"
     "sync"
+    "regexp"
+    "fmt"
     "github.com/urfave/cli"
     "github.com/yuhangwang/gotask/read"
     "github.com/yuhangwang/gotask/encode"
@@ -18,10 +20,20 @@ func main() {
     app.Version = "0.1.0"
     app.Usage = "Command line parallel task manager"
     app.Action = func(context *cli.Context) error {
+        if len(context.Args()) == 0 {
+            fmt.Println("Error hint: please provide arguments to gotask")
+            os.Exit(1)
+        }
+        validYamlFileName := regexp.MustCompile(".*[.]yaml$")
+        cmd := context.Args().First()
         argc := context.NArg()
         file_tasks := context.Args().Get(argc-1)
+        if !validYamlFileName.MatchString(file_tasks) {
+            fmt.Println("Error hint: please provide a YAML file")
+            task.Run(cmd, context.Args()[1:]...)
+            os.Exit(1)
+        }
         data := read.Yaml(file_tasks)
-        cmd := context.Args().First()
         primary_arg := context.Args()[1:argc-1]
         var wg sync.WaitGroup
         for _, v := range data.([]interface{}) {
